a3f40178522ff25d594e106429700eb0831aa09b
Rimberse <kerimpltm@gmail.com>
2023-04-02 04:45:39 +0200

    Add project dependencies, 3rd party libraries & do preliminary configurations

--- app/src/main/java/net/efrei/hudayberdiyevkerim/acetrack/MainActivity.kt
+++ app/src/main/java/net/efrei/hudayberdiyevkerim/acetrack/MainActivity.kt
@@ -1,43 +1,11 @@
 package net.efrei.hudayberdiyevkerim.acetrack
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import net.efrei.hudayberdiyevkerim.acetrack.ui.theme.AceTrackTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AceTrackTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AceTrackTheme {
-        Greeting("Android")
+        setContentView(R.layout.main_activity);
     }
 }
