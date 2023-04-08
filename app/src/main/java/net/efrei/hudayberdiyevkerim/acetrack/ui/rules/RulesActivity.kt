b8c3b1c161a36cdb6274df5a484fe4d268b6206f
Rimberse <kerimpltm@gmail.com>
2023-04-08 21:23:51 +0200

    Create Rules & Regulations layout, bind it to Rules Activity

--- app/src/main/java/net/efrei/hudayberdiyevkerim/acetrack/ui/rules/RulesActivity.kt
+++ app/src/main/java/net/efrei/hudayberdiyevkerim/acetrack/ui/rules/RulesActivity.kt
@@ -2,13 +2,17 @@ package net.efrei.hudayberdiyevkerim.acetrack.ui.rules
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import net.efrei.hudayberdiyevkerim.acetrack.databinding.ActivityRulesBinding
 import net.efrei.hudayberdiyevkerim.acetrack.main.MainApp
 
 class RulesActivity : AppCompatActivity() {
     lateinit var app: MainApp
+    private lateinit var binding: ActivityRulesBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityRulesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         app = application as MainApp
     }
