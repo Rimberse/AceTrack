87692bb6163f04a24d7daa3dbd90b8038356de60
Rimberse <kerimpltm@gmail.com>
2023-04-08 19:58:53 +0200

    Implement more menu and add it to top app bar

--- app/src/main/java/net/efrei/hudayberdiyevkerim/acetrack/ui/authentication/WelcomeActivity.kt
+++ app/src/main/java/net/efrei/hudayberdiyevkerim/acetrack/ui/authentication/WelcomeActivity.kt
@@ -1,14 +1,24 @@
 package net.efrei.hudayberdiyevkerim.acetrack.ui.authentication
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import net.efrei.hudayberdiyevkerim.acetrack.R
 import net.efrei.hudayberdiyevkerim.acetrack.databinding.ActivityWelcomeBinding
 import net.efrei.hudayberdiyevkerim.acetrack.main.MainApp
 
 class WelcomeActivity: AppCompatActivity() {
     lateinit var app: MainApp
     private lateinit var binding: ActivityWelcomeBinding
+
+    fun displayRules(item: MenuItem) {
+        val intent = Intent(this, WelcomeActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +39,11 @@ class WelcomeActivity: AppCompatActivity() {
         }
 
         app = application as MainApp
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bar_top_app, menu)
+        return true
     }
 
     override fun onStart() {
