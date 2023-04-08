eb4f0ac832a3528050f8a0f7a54322606168edb3
Rimberse <kerimpltm@gmail.com>
2023-04-08 16:11:38 +0200

    Implement view binding for Welcome activity, to dynamically load the layout & display it on screen

--- app/src/main/java/net/efrei/hudayberdiyevkerim/acetrack/ui/authentication/WelcomeActivity.kt
+++ app/src/main/java/net/efrei/hudayberdiyevkerim/acetrack/ui/authentication/WelcomeActivity.kt
@@ -1,15 +1,32 @@
 package net.efrei.hudayberdiyevkerim.acetrack.ui.authentication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import net.efrei.hudayberdiyevkerim.acetrack.R
+import net.efrei.hudayberdiyevkerim.acetrack.databinding.ActivityWelcomeBinding
 import net.efrei.hudayberdiyevkerim.acetrack.main.MainApp
 
 class WelcomeActivity: AppCompatActivity() {
     lateinit var app: MainApp
+    private lateinit var binding: ActivityWelcomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.topAppBar)
+
+        binding.topAppBar.setNavigationOnClickListener {
+            Log.i("Top app bar navigation button","clicked")
+        }
+
+        binding.loginButton.setOnClickListener {
+            Log.i("Login button","clicked")
+        }
+
+        binding.registerButton.setOnClickListener {
+            Log.i("Register button","clicked")
+        }
 
         app = application as MainApp
     }
