ff2b119cef94ac845be56553f21a5562e1f38ef1
Rimberse <kerimpltm@gmail.com>
2023-04-16 15:44:03 +0200

    Add App bar to Home activity

--- app/src/main/java/net/efrei/hudayberdiyevkerim/acetrack/ui/home/HomeActivity.kt
+++ app/src/main/java/net/efrei/hudayberdiyevkerim/acetrack/ui/home/HomeActivity.kt
@@ -3,9 +3,13 @@ package net.efrei.hudayberdiyevkerim.acetrack.ui.home
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import net.efrei.hudayberdiyevkerim.acetrack.R
 import net.efrei.hudayberdiyevkerim.acetrack.databinding.ActivityHomeBinding
 import net.efrei.hudayberdiyevkerim.acetrack.ui.authentication.LoggedInViewModel
@@ -22,6 +26,9 @@ class HomeActivity : AppCompatActivity() {
         homeBinding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(homeBinding.root)
         drawerLayout = homeBinding.drawerLayout
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         val navView = homeBinding.navView
     }
