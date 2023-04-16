c59ec88abeb60e79d521bf8aaba29b218f40e3b3
Rimberse <kerimpltm@gmail.com>
2023-04-16 20:12:02 +0200

    Implement Log out functionality, to permit users ending their sessions

--- app/src/main/java/net/efrei/hudayberdiyevkerim/acetrack/ui/home/HomeActivity.kt
+++ app/src/main/java/net/efrei/hudayberdiyevkerim/acetrack/ui/home/HomeActivity.kt
@@ -2,6 +2,7 @@ package net.efrei.hudayberdiyevkerim.acetrack.ui.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -77,6 +78,13 @@ class HomeActivity : AppCompatActivity() {
                 .centerCrop()
                 .into(navHeaderBinding.navHeaderImage)
         }
+    }
+
+    fun logOut(item: MenuItem) {
+        loggedInViewModel.logOut()
+        val intent = Intent(this, WelcomeActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
     }
 
     override fun onSupportNavigateUp(): Boolean {
