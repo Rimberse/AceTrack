bf198e4ad299058f55072b1f9078edbea4b566f1
Rimberse <kerimpltm@gmail.com>
2023-04-26 14:35:36 +0200

    Update LocalService, change code

--- app/src/main/java/net/efrei/hudayberdiyevkerim/acetrack/ui/settings/SettingsActivity.kt
+++ app/src/main/java/net/efrei/hudayberdiyevkerim/acetrack/ui/settings/SettingsActivity.kt
@@ -1,13 +1,11 @@
 package net.efrei.hudayberdiyevkerim.acetrack.ui.settings
 
 import android.R
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.view.View.OnClickListener
 import androidx.appcompat.app.AppCompatActivity
-import net.efrei.hudayberdiyevkerim.acetrack.databinding.ActivityRulesBinding
+import androidx.core.content.ContentProviderCompat
 import net.efrei.hudayberdiyevkerim.acetrack.databinding.ActivitySettingsBinding
 import net.efrei.hudayberdiyevkerim.acetrack.helpers.LocaleService
 import net.efrei.hudayberdiyevkerim.acetrack.main.MainApp
@@ -27,15 +25,15 @@ class SettingsActivity : AppCompatActivity() {
             startActivity(Intent(this, WelcomeActivity::class.java))
         }
 
-        binding.englishButton.setOnClickListener {
-            LocaleService.changeLanguage(this, "en")
+        binding.englishButton.setOnClickListener(OnClickListener {
+            LocaleService.setLocale(this, "en")
             recreate()
-        }
+        })
 
-        binding.frenchButton.setOnClickListener {
-            LocaleService.changeLanguage(this, "fr")
+        binding.frenchButton.setOnClickListener(OnClickListener {
+            LocaleService.setLocale(this, "fr")
             recreate()
-        }
+        })
 
         app = application as MainApp
     }
